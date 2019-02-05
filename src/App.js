3bff3518048cea35c11e6fd07da6c3b9a04c618d
Oleksandr <targonsky97@gmail.com>
2019-02-05 16:59:52 +0200

    style: add font

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component }  from 'react';
 
-import routes               from './routes';
+import routes                from './routes';
+import { createGlobalStyle } from 'styled-components';
+
 import './App.css';
 
 class App extends Component {
@@ -8,10 +10,19 @@ class App extends Component {
   render() {
     return (
       <div>
+        <GlobalStyle />
         {routes}
       </div>
     );
   }
 }
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding     : 0;
+    margin      : 0;
+    font-family : 'Raleway', sans-serif;
+  }
+`;
 
 export default App;
