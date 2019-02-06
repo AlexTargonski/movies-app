63bbce3f38ee80f405aae2389dd51bd49b724d17
Oleksandr <targonsky97@gmail.com>
2019-02-06 16:39:55 +0200

    feat: add route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,13 @@ import {
 }                              from 'react-router-dom';
 
 import HomePage                from '../pages/HomePage';
+import MovieDetailsPage        from '../pages/MovieDetailsPage';
 
 export default (
   <Router>
     <Switch>
-      <Route exact path="/" component={HomePage} />
+      <Route exact path="/"           component={HomePage} />
+      <Route exact path="/movies/:id" component={MovieDetailsPage} />
     </Switch>
   </Router>
 );
